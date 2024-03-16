a7cab6268dde151eeefef7ce9498c96af61f4432
Shuyeb <149717496+shuyeb00@users.noreply.github.com>
2024-03-16 20:25:21 +0600

    Create biscuit_generator.cpp

---
+++ week_1/day_2/biscuit_generator.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define code() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main()
+{
+    code();
+    
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    cout<<(c/a)*b<<endl;
+
+    return 0;
+}
